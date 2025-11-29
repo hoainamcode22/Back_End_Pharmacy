5fb992217eb1ed076d24c63b9ae3b5e6029d83fe
hoainamcode22 <namhoai2208204@gmail.com>
2025-11-29 09:52:36 +0700

    bổ sung

--- Front_end/src/config.js
+++ Front_end/src/config.js
@@ -1,2 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5001/api";
+//export const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5001/api";
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5001";
+
+const API_BASE_URL = 'https://be-1-kh9g.onrender.com';
